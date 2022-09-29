1fdf25c5bdb8d3b5ab6e2c09da1909405656223b
devcoderoad <devdef20@gmail.com>
2022-09-29 21:44:05 +0700

    chore: updates

--- pages/charts.vue
+++ pages/charts.vue
@@ -109,12 +109,8 @@
                               hoverOffset: 4,
                               rotation: 90,
                               cutout: 10,
-                              radius: `50%`,
                               hoverOffset: 30,
-                              hoverBorderWidth: 20,
-                              layout: {
-                                padding: 20
-                              }
+                              hoverBorderWidth: 20
                             }
                           ]"
                           :height="90"
@@ -255,29 +251,25 @@ export default {
           '2019-08',
           '2019-09',
           '2019-10',
-          '2019-11',
-          '2019-12',
-          '2020-01',
-          '2020-02',
-          '2020-03'
+          '2019-11'
         ],
         datasets: [
           {
             label: 'Visualization',
-            data: [72, 131, 12, 3, 4, 55, 4, 10, 20],
+            data: [72, 131, 12, 3, 4, 55],
             backgroundColor: [
               '#99aa00',
               '#aabbee',
               '#990000',
               '#99ff00',
               '#994400',
-              '#9900ff',
-              '#9955ff',
-              '#0099ff',
-              '#ee99ff'
+              '#9900ff'
             ],
             borderColor: 'rgba(255, 255, 255, 1)',
-            borderWidth: 2
+            borderWidth: 2,
+            radius: 240,
+            hoverBackgroundColor: 'rgba(100, 0, 0, 0.5)',
+            hoverOffset: 35
           }
         ]
       },
